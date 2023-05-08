ece5caf1a5f1390a9400ee3fe6da4c475a4e0506
aqeme <aq.eme@yandex.ru>
2023-05-08 20:23:01 +0300

    Sprint 5

---
+++ search-server/document.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "paginator.h"
+
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+struct Document {
+    Document() = default;
+    Document(int id, double relevance, int rating)
+            : id(id)
+            , relevance(relevance)
+            , rating(rating) {
+    }
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
---
+++ search-server/string_processing.cpp
@@ -0,0 +1,32 @@
+#include "string_processing.h"
+
+using namespace std::literals;
+
+std::ostream& operator<<(std::ostream& out, const Document& document) {
+    out << "{ "s
+        << "document_id = "s << document.id << ", "s
+        << "relevance = "s << document.relevance << ", "s
+        << "rating = "s << document.rating << " }"s;
+    return out;
+}
+
+
+std::vector<std::string> SplitIntoWords(const std::string& text) {
+    std::vector<std::string> words;
+    std::string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        } else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+    return words;
+}
+
